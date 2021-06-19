df2db1ee6cf8a250784c7d559a6235b9c6cb2328
osmanerol <osmanerol98@outlook.com>
2021-06-19 22:03:14 +0300

    canvas drawer clear and undo added

--- frontend/src/pages/DrawerPage/index.tsx
+++ frontend/src/pages/DrawerPage/index.tsx
@@ -1,27 +1,33 @@
-import React, { useState, FC, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import './index.scss';
 import { Container } from 'react-bootstrap';
 import CanvasDraw from "react-canvas-draw";
 import { Button } from '../../components';
 
 const Index = () => {
-    const [showEmpty, setShowEmpty] = useState(true);
-    const canvas = useRef(null);
+    const canvas = useRef<any>();
 
     const clearCanvas = () => {
-        //canvas.current!.clear();
+        canvas.current!.clear();
+    }
+
+    const undoCanvas = () => {
+        canvas.current!.undo();
     }
     
     return (
         <Container className='drawer-page-container'>
-            <Button text='Temizle' className='col-md-3 pl-0 py-2' onClick={clearCanvas} />
+            <div className="row py-2">
+                <Button text='Temizle' className='col-md-2 col-sm-4 col-6 clear-button' size='sm' onClick={clearCanvas} />
+                <Button text='Geri al' className='col-md-2 col-sm-4 col-6 undo-button' size='sm' onClick={undoCanvas} />
+            </div>
             <CanvasDraw 
+                ref={canvasDrawer => { canvas.current = canvasDrawer }}
                 brushRadius={3}
                 brushColor='#444'
                 canvasWidth={600}
                 canvasHeight={600}
                 imgSrc={localStorage.getItem('image') ? localStorage.getItem('image')! : ""}
-                ref={canvas}
             />
         </Container>
     );
